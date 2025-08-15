6b87abaa7d4f200f9b19e0e2a3f46a18cfda03ed
Bùi Thiện <21110656@student.hcmute.edu.vn>
2025-08-15 18:10:30 +0700

    Initial commit

---
+++ src/main/java/com/clutchhotel/HotelClutch/HotelClutchApplication.java
@@ -0,0 +1,13 @@
+package com.clutchhotel.HotelClutch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelClutchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelClutchApplication.class, args);
+	}
+
+}
